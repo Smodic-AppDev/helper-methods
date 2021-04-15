efbc15ec57620b1a079042aea5cfa7f255d19a18
dsmodic <dsmodic@chicagobooth.edu>
2021-04-15 01:05:43 +0000

    Updated syntax to be conventional

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,18 +1,17 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
 
-    #render template: "movies/new.html.erb" Not needed based Rails assumptions
+    #render template: "movies/new.html.erb" Not needed based on Rails assumptions
   end
 
   def index
-    matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @movies = Movie.order(created_at: :desc)
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
       format.html
@@ -21,22 +20,26 @@ class MoviesController < ApplicationController
   end
 
   def show
+    # @movie = Movie.where(id: params.fetch(:id)).first    replaced by below
+    # @movie = Movie.find_by(id: params.fetch(:id))
+    @movie = Movie.find(params.fetch(:id))
+    
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where( id: the_id )
 
-    @the_movie = matching_movies.first
+    @movie = matching_movies.first
 
     #render template: "movies/show.html.erb"  Not needed based on Rails assumptions
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    @movie = Movie.new
+    @movie.title = params.fetch("title")
+    @movie.description = params.fetch("description")
 
-    if @the_movie.valid?
-      @the_movie.save
+    if @movie.valid?
+      @movie.save
       redirect_to movies_url, notice: "Movie created successfully."
     else
       render "new" # matches controller name but doesn't match action so tempalte name is needed
@@ -44,36 +47,30 @@ class MoviesController < ApplicationController
   end
 
   def edit
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id))
 
     #render template: "movies/edit.html.erb"  Not needed based on Rails assumptions
   end
 
   def update
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    @movie = Movie.find(params.fetch(:id))
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    @movie.title = params.fetch("title")
+    @movie.description = params.fetch("description")
 
-    if the_movie.valid?
-      the_movie.save
-      redirect_to(movie_url(the_movie), { notice: "Movie updated successfully."} )
+    if @movie.valid?
+      @movie.save
+      redirect_to(movie_url(@movie),  notice: "Movie updated successfully." )
     else
-      redirect_to(movie_url(the_movie), { alert: "Movie failed to update successfully." })
+      redirect_to(movie_url(@movie),  alert: "Movie failed to update successfully." )
     end
   end
 
   def destroy
-    the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    @movie = Movie.find(params.fetch(:id))
 
-    the_movie.destroy
+    @movie.destroy
 
-    redirect_to(movies_url, { notice: "Movie deleted successfully."} )
+    redirect_to(movies_url,  notice: "Movie deleted successfully." )
   end
 end
